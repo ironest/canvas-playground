2c553d9f8fe409f520bff9b1d9655aaeb8be64dd
ironest <ironest@gmail.com>
2020-01-07 16:03:56 +1100

    Updated Main core App

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import Canvas from "./Canvas"
 
 class App extends Component {
 
@@ -6,6 +7,7 @@ class App extends Component {
         return(
             <div>
                 <h1>Welcome To Your Drawing Pad!</h1>
+                <Canvas />
             </div>
         )
     }
